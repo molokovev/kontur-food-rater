96597361f30d977515f78e9d4d97bf73e0a33789
Eugene Molokov <molokovev@gmail.com>
2016-01-05 22:31:54 +0500

    don't show pageAction icon for the time being; increase extension version; remove some trash;

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-var SITE_NAME = 'cafe.skbkontur.ru';
-
-// Call the above function when the url of a tab changes.
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.indexOf(SITE_NAME) !== -1) {
-    chrome.pageAction.show(tabId);
-  }
-});
+//var SITE_NAME = 'cafe.skbkontur.ru';
+//
+//chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//  if (tab.url.indexOf(SITE_NAME) !== -1) {
+//    chrome.pageAction.show(tabId);
+//  }
+//});
